0cdfaefb305eae49ba423e22265c0179c19d0a2a
gwen adomaitis <gwen adomaitis>
2017-08-06 09:59:12 -0400

    Initial Commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	[SerializeField] int RoomNumber = 8;
+	[SerializeField] float LevelWidth = 50;
+	[SerializeField] float LevelLength = 70;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
