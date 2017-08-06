c45b92f3fb102bec63dff43f37cd1a543d95c53e
gwen adomaitis <gwen adomaitis>
2017-08-06 12:05:57 -0400

    Added Vertices for the Grid

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour {
+[RequireComponent (typeof(MeshRenderer), typeof(MeshFilter))]
+public class Grid : MonoBehaviour
+{
+    public int xSize, ySize;
+    public float waitLength = 0.05f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Vector3[] vertices;
+
+    void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+
+    IEnumerator Generate()
+    {
+        WaitForSeconds wait = new WaitForSeconds(waitLength);
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+
+        for(int i = 0, y = 0; y <= ySize; y++)
+        {
+            for(int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                yield return wait;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if(vertices == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.black;
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
 }
